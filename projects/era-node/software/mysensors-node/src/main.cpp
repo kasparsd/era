da7e6ffe7bac750db993de753a46320a648b3117
Kaspars Dambis <hi@kaspars.net>
2022-01-24 10:40:54 +0200

    Document the current limitations

--- projects/era-node/software/mysensors-node/src/main.cpp
+++ projects/era-node/software/mysensors-node/src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
 
+// Debug output is printed on the RS485 bus too since they share the same UART port.
 #define MY_DEBUG
+#define MY_BAUD_RATE 9600
+#define MY_DISABLED_SERIAL // Disable debug output to allow using UART0 for RS485.
+
+// These must be hardcoded because MySensors version 2.3.2 has a bug
+// that prevents the node ID assignment, see https://github.com/mysensors/MySensors/issues/1450
+#define MY_NODE_ID 1
+#define MY_PARENT_NODE_ID 0 // Don't attempt to find it.
+#define MY_PARENT_NODE_IS_STATIC // If the controller doesn't support returning the parent node.
+
 #define MY_RS485
 #define MY_RS485_DE_PIN D0
 #define MY_RS485_BAUD_RATE 9600
 #define MY_RS485_HWSERIAL Serial
-
-// Use the Wemos D1 LED for status.
-#define MY_DEFAULT_RX_LED_PIN D4
-#define MY_DEFAULT_TX_LED_PIN D4
+#define MY_RS485_SOH_COUNT 3 // Avoid collisions on the RS485 bus.
 
 #include <MySensors.h>
 
@@ -32,6 +39,6 @@ void presentation() {
 void loop() {
 	send(msg.set(state ? "1" : "0"));
 	state = state ? false : true;
-	delay(5000);
+	wait(1000);
 }
  
