d806ad19657622d6141186f6a07fdac1d0df56ca
Kaspars Dambis <hi@kaspars.net>
2022-04-23 22:17:43 +0300

    Make it a sensor for flexibilityFor readability

--- projects/era-node/software/esphome-node/modbus_rebel_switch.h
+++ projects/era-node/software/esphome-node/modbus_rebel_switch.h
@@ -16,13 +16,15 @@ class ModbusRebelSwitch : public Component, public Switch {
 
       payload.push_back(state);
 
-      // uint8_t address,
-      // uint8_t function_code, 
-      // uint16_t start_address, 
-      // uint16_t number_of_entities,
-      // uint8_t payload_len = 0,
-      // const uint8_t *payload = nullptr
-      modbus->send(3, 5, 6, 1, payload.size(), &payload[0]);
+	  // Send the message.
+      modbus->send(
+		  3, // uint8_t address
+		  0x05, // uint8_t function_code - WRITE_SINGLE_COIL
+		  6, // uint16_t start_address
+		  1, // uint16_t number_of_entities
+		  payload.size(), // uint8_t payload_len = 0
+		  &payload[0] // const uint8_t *payload = nullptr
+		);
 
       // Acknowledge new state by publishing it.
       publish_state(state);
